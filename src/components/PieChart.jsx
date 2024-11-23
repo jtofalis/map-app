3311c53d200389666c592c3c1fb69a022d72ba68
GoudekettingRM <r.goudeketting@gmail.com>
2024-11-24 00:45:33 +0100

    Add switch and remove summary

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -10,13 +10,17 @@ const PieChartByRobinHeIsAmazing = ({ savedPositions }) => {
   const shortValue = categories.filter((category) => category === 'short').length;
 
   return (
+    <div style={{width: '100%', display: 'flex', minHeight: '350px'}}>
+
     <PieChart
+      style={{maxWidth: '200px', marginInline: 'auto'}}
       data={[
         { title: 'Long', value: longValue, color: '#E38627' },
         { title: 'Medium', value: mediumValue, color: '#C13C37' },
         { title: 'Short', value: shortValue, color: '#6A2135' },
       ]}
-    />
+      />
+      </div>
   );
 };
 export default PieChartByRobinHeIsAmazing;
