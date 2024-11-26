2737d505e01ce834a6b624bc281edf1190fbf792
John Tofalis <jptofalis@hotmail.co.uk>
2024-11-26 15:23:48 +0000

    Adjustments

--- src/components/Pitch/FrisbeePitch.jsx
+++ src/components/Pitch/FrisbeePitch.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import Cone from './Cone';
 import EndZone from './EndZone';
 
 const FrisbeePitch = ({ children }) => {
-  const conePositions = [
-    'top-left',
-    'top-right',
-    'bottom-left',
-    'bottom-right',
-    'endzone-top-left',
-    'endzone-top-right',
-    'endzone-bottom-left',
-    'endzone-bottom-right',
-  ];
 
   return (
     <div className='px-2'>
-      <div className='max-w-sm h-[60vh] bg-[#6BA368] relative mx-auto'>
+      <div className='max-w-xs drop-shadow-xl h-[60vh] bg-[#6BA368] relative mx-auto border-4 border-slate-300'>
         {/* End Zones */}
         <EndZone position='top-0' text='Attacking End Zone' />
         <EndZone position='bottom-0' text='Defending End Zone' />
 
         {/* Midline */}
-        <div className='w-full h-0.5 bg-white absolute top-1/2 left-0 -translate-y-1/2' />
+        <div className='w-full h-0.5 bg-slate-300 absolute top-1/2 left-0 -translate-y-1/2' />
 
-        {/* Cones */}
-        {conePositions.map((position) => (
-          <Cone key={position} position={position} />
-        ))}
+        {/* End Zone Borders */}
+        <div className='w-full h-0.5 bg-slate-300 absolute top-[20%] left-0' />
+        <div className='w-full h-0.5 bg-slate-300 absolute bottom-[20%] left-0' />
 
         {children}
       </div>
